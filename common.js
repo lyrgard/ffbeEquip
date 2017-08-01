9aac00c0da6ea8b8b8d64b9a75564a1fe3cbcf3e
Lyrgard <lyrgard@gmail.com>
2017-08-01 15:40:36 +0200

    next part of work on contribute

--- common.js
+++ common.js
@@ -6,10 +6,34 @@ var baseStats = ['hp','mp','atk','def','mag','spr'];
 var filters = ["types","elements","ailments","killers","accessToRemove","additionalStat"];
 var elementList = ['fire','ice','lightning','water','earth','wind','light','dark'];
 var ailmentList = ['poison','blind','sleep','silence','paralysis','confuse','disease','petrification'];
+var killerList = ['aquatic','beast','bird','bug','demon','dragon','human','machine','plant','undead','stone','spirit'];
 var typeList = ["dagger", "sword", "greatSword", "katana", "staff", "rod", "bow", "axe", "hammer", "spear", "harp", "whip", "throwing", "gun", "mace", "fist", "lightShield", "heavyShield", "hat", "helm", "clothes", "robe", "lightArmor", "heavyArmor", "accessory", "materia"];
+var accessList = ["shop","chest","quest","trial","chocobo","event","colosseum","key","TMR-1*","TMR-2*","TMR-3*","TMR-4*","TMR-5*","recipe-shop","recipe-chest","recipe-quest","recipe-event","recipe-colosseum","recipe-key","trophy","recipe-trophy"];
 
 function getElementHtml(element) {
     return "<div class='specialImg'><img class='miniIcon' src='img/sword.png'></img><img src='img/" + element + ".png'></img></div>"
+}
+
+function getAilmentsHtml(item) {
+    var html = "";
+    $(item.ailments).each(function(index, ailment) {
+        html += "<div class='specialImg noWrap ailment-" + ailment + "'><img class='miniIcon' src='img/sword.png'></img><img class='imageWithText' src='img/" + ailment.name + ".png'></img>" + ailment.percent + "%</div>";
+    });
+    return html;
+}
+function getResistHtml(item) {
+    var html = "";
+    $(item.resist).each(function(index, resist) {
+        html += "<div class='specialImg noWrap resist-" + resist.name + "'><img class='miniIcon' src='img/heavyShield.png'></img><img class='imageWithText' src='img/" + resist.name + ".png'></img>" + resist.percent + "%</div>";
+    });
+    return html;
+}
+function getKillersHtml(item) {
+    var html = "";
+    $(item.killers).each(function(index, killer) {
+        html += "<div class='specialImg noWrap killer-" + killer.name + "'><img class='imageWithText' src='img/killer.png'></img>" + killer.name + " " + killer.percent + "%</div>";
+    });
+    return html;
 }
 
 // Create an HTML span containing the stats of the item
@@ -64,4 +88,10 @@ function isNumber(evt) {
         return false;
     }
     return true;
+};
+
+function isEnter(evt) {
+    evt = (evt) ? evt : window.event;
+    var charCode = (evt.which) ? evt.which : evt.keyCode;
+    return charCode == 13;
 };
