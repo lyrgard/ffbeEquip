340522b51c4ea7cb47a8b6dbcd4ad2acb6c2d8f5
Lyrgard <lyrgard@gmail.com>
2017-12-19 11:29:52 +0100

    added unit id to export as CSV

--- static/inventory.js
+++ static/inventory.js
@@ -372,12 +372,12 @@ function prepareLastItemReleases() {
 }
 
 function exportAsCsv() {
-    var csv = "Item Id; Item Name;Number Owned;TMR of;\n";
+    var csv = "Item Id;Item Name;Item typeNumber Owned;TMR of;\n";
     var sortedItems = sort(equipments).concat(sort(materia));
     for (var index = 0, len = sortedItems.length; index < len; index++) {
         var item = sortedItems[index];
         if (itemInventory[item[itemKey]]) {
-            csv +=  "\"" + item.id + "\";" + "\"" + item.name + "\";" + itemInventory[item[itemKey]] + ';\"' + (item.tmrUnit ? item.tmrUnit : "") + "\"\n";
+            csv +=  "\"" + item.id + "\";" + "\"" + item.name + "\";" + "\"" + item.type + "\";" + itemInventory[item[itemKey]] + ';\"' + (item.tmrUnit ? item.tmrUnit : "") + "\"\n";
         }
     }
     window.saveAs(new Blob([csv], {type: "text/csv;charset=utf-8"}), 'FFBE_Equip - Unit collection.csv');
