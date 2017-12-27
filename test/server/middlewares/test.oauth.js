4a45948ba1d3df814f730c8a52c3996c0fec1bde
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2017-12-27 18:39:32 +0100

    test(server): add oauth middleware unit test

---
+++ test/server/middlewares/test.oauth.js
@@ -0,0 +1,47 @@
+const assert = require('power-assert');
+const request = require('supertest');
+const express = require('express');
+const sessions = require('client-sessions');
+const authRequired = require('../../../server/middlewares/oauth.js');
+
+describe('middlewares.oauth', () => {
+  const app = express();
+  const agent = request.agent(app);
+
+  // Middleware
+  app.use(sessions({
+    cookieName: 'OAuthSession',
+    secret: 'yo',
+  }));
+
+  // Routes
+  app.get('/secure', authRequired, (req, res) => {
+    const { tokens } = req.OAuthSession;
+    return res.status(200).send(tokens);
+  });
+  app.get('/authenticate', (req, res) => {
+    req.OAuthSession.tokens = 'test';
+    return res.status(200).send();
+  });
+
+  it('.unauthorized', (done) => {
+    request(app)
+      .get('/secure')
+      .expect(401, done);
+  });
+
+  it('.authenticate', (done) => {
+    agent
+      .get('/authenticate')
+      .expect((res) => {
+        assert.equal(res.headers['set-cookie'].length, 1);
+      })
+      .end(done);
+  });
+
+  it('.authorized', (done) => {
+    agent
+      .get('/secure')
+      .expect(200, 'test', done);
+  });
+});
