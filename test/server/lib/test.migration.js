8ff25070866a153ffd14677a4d5e95051db014cc
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2018-02-17 19:58:21 +0100

    test(server): add migration test

---
+++ test/server/lib/test.migration.js
@@ -0,0 +1,47 @@
+const assert = require('power-assert');
+const migration = require('../../../server/lib/migration.js');
+
+describe('lib.migration', () => {
+  it('.missing schema', () => {
+    const oldData = { d: '1234' };
+    const newData = migration.up('missing', oldData);
+    assert.equal(newData, oldData);
+  });
+
+  it('.itemInventory v1', () => {
+    const old = {
+      Excalipoor: 1,
+      'Save the Queen': 11,
+      unrelated: 'test',
+      version: 1,
+    };
+    const inventory = migration.up('itemInventory', old);
+    assert.deepEqual(inventory, {
+      303001400: 11,
+      303001600: 1,
+      unrelated: 'test',
+      version: 3,
+    });
+  });
+
+  it('.itemInventory v2', () => {
+    const old = {
+      303003500: 12,
+      version: 2,
+    };
+    const inventory = migration.up('itemInventory', old);
+    assert.deepEqual(inventory, {
+      303001400: 12,
+      version: 3,
+    });
+  });
+
+  it('.itemInventory v3', () => {
+    const old = {
+      303001400: 12,
+      version: 10,
+    };
+    const inventory = migration.up('itemInventory', old);
+    assert.deepEqual(inventory, old);
+  });
+});
