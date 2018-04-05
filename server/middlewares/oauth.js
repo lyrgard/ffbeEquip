ef4a976cca757ca7e857b5a0c0126f2d054f2a8d
Lyrgard <lyrgard@gmail.com>
2018-04-05 15:25:08 +0200

    update GL data after maintenance

--- server/middlewares/oauth.js
+++ server/middlewares/oauth.js
@@ -3,12 +3,16 @@ const OAuth = require('../lib/oauth.js');
 module.exports = (req, res, next) => {
   const { tokens } = req.OAuthSession;
   if (!tokens) {
+      console.log("No token found, send beck 401");
     return res.status(401).send();
   }
 
   req.OAuth2Client = OAuth.createClient(tokens);
 
   return req.OAuth2Client.getRequestMetadata(null, (error) => {
+      console.log("Error : ");
+      console.log(error);
+      console.log(req.OAuth2Client.credentials);
     req.OAuthSession.tokens = req.OAuth2Client.credentials;
     next(error);
   });
