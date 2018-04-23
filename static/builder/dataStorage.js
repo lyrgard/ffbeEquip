4011251cbfc95ac9ec87ffaf34f9382161ee5ef8
Lyrgard <lyrgard@gmail.com>
2018-04-23 13:25:37 +0200

    keep items of desired type even if not contributing directly to goal

--- static/builder/dataStorage.js
+++ static/builder/dataStorage.js
@@ -45,10 +45,23 @@ class DataStorage {
     setUnitBuild(unitBuild) {
         this.unitBuild = unitBuild;
         this.desirableElements = [];
+        this.desiredEquipmentType = [];
         for (var index = 0, len = this.unitBuild.unit.skills.length; index < len; index++) {
             var skill = this.unitBuild.unit.skills[index];
             if (skill.equipedConditions && skill.equipedConditions.length == 1 && elementList.includes(skill.equipedConditions[0]) && !this.desirableElements.includes(skill.equipedConditions[0])) {
-                this.desirableElements.push(skill.equipedConditions[0]);
+                
+            }
+            if (skill.equipedConditions) {
+                for (var i = skill.equipedConditions.length; i--;) {
+                    var condition = skill.equipedConditions[i];
+                    if (elementList.includes(condition)) {
+                        if (!this.desirableElements.includes(condition)) {
+                            this.desirableElements.push(skill.equipedConditions[0]);    
+                        }
+                    } else if (!this.desiredEquipmentType.includes(condition)) {
+                        this.desiredEquipmentType.push(condition);
+                    }
+                }
             }
         }
         this.prepareAllItemsVersion();
@@ -223,6 +236,9 @@ class DataStorage {
         }
         if (this.desirableElements.length != 0) {
             if (item.element && matches(item.element, this.desirableElements)) return true;
+        }
+        if (this.desiredEquipmentType.length != 0) {
+            if (item.type && this.desiredEquipmentType.includes(item.type)) return true;
         }
     }
     
