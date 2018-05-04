09c2222f017ab5125c5e85f5728a37ee6359f5bb
Lyrgard <lyrgard@gmail.com>
2018-05-04 15:48:07 +0200

    some server changes

--- server/middlewares/oauth.js
+++ server/middlewares/oauth.js
@@ -2,7 +2,6 @@ const OAuth = require('../lib/oauth.js');
 
 module.exports = (req, res, next) => {
   const { tokens } = req.OAuthSession;
-    console.log(tokens);
   if (!tokens) {
       console.log("No token found, send beck 401");
     return res.status(401).send();
@@ -11,8 +10,6 @@ module.exports = (req, res, next) => {
   req.OAuth2Client = OAuth.createClient(tokens);
 
   return req.OAuth2Client.getRequestMetadata(null, (error) => {
-    console.log("Refreshed Token");
-      console.log(req.OAuth2Client.credentials);
     req.OAuthSession.tokens = req.OAuth2Client.credentials;
     next(error);
   });
