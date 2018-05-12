d8941a55966058e1983212eb18566c5d981eca9d
Lyrgard <lyrgard@gmail.com>
2018-05-12 15:49:46 +0200

    fix refresh token problem

--- server/routes/oauth.js
+++ server/routes/oauth.js
@@ -2,6 +2,7 @@ const Joi = require('joi');
 const Boom = require('boom');
 const express = require('express');
 const validator = require('../middlewares/validator.js');
+const drive = require('../lib/drive.js');
 const OAuth = require('../lib/oauth.js');
 
 const route = express.Router();
@@ -23,14 +24,28 @@ const callbackSchema = Joi.object({
 route.get('/googleOAuthSuccess', validator.query(callbackSchema), (req, res, next) => {
   const { state, code } = req.query;
 
-  OAuth.client.getToken(code, (err, tokens) => {
+  OAuth.client.getToken(code, async (err, tokens) => {
     if (err) {
       return next(Boom.boomify(err, {
         statusCode: err.code,
       }));
     }
-    console.log(tokens);
     req.OAuthSession.tokens = tokens;
+    const auth = OAuth.createClient(tokens);
+    if (tokens.refresh_token) {
+        await drive.writeJson(auth, 'refreshToken.json', {"refreshToken": tokens.refresh_token});
+        console.log("refresh token writen to drive");
+    } else {
+        let refreshTokenData = await drive.readJson(auth, 'refreshToken.json', {});
+        if (!refreshTokenData.refreshToken) {
+            console.log("redirect to consent to get refresh token");
+            return res.redirect(OAuth.authUrlConsent + "&state=" + encodeURIComponent(state));
+        } else {
+            req.OAuthSession.tokens.refresh_token = refreshTokenData.refreshToken;
+            console.log("refresh token read from drive");
+        }
+    }
+    console.log(req.OAuthSession.tokens)
     return res.redirect(state);
   });
 });
