cfea6cefc101e1d2c5f3b8276fd6c6b9ba6d4f65
Jonathan Dion <jonathan.dion@agfa.com>
2018-05-17 14:26:46 +0200

    added D, C and B rank expeditions preset goals

--- tools/readExpeditions.js
+++ tools/readExpeditions.js
@@ -1,37 +1,66 @@
-var data = "General Hospitality;A;;55.1;;;;29.39;White Magic: 7;+4%;16%|"+
-"Stranger Stuff;A;;55.1;;;29.39;;Black Magic: 7;+4%;16%|"+
-"He Who Smelt It;A;2142;53.55;;;85.68;85.68;Green Magic: 7;+4%;16%|"+
-"Full House;A;2164;54.1;;;;43.28;Equipable: Harp;+4%;16%|"+
-"A Beast's Bounty;A;;108.2;43.28;;43.28;;Equipable: Whip;+4%;16%|"+
-"Child's Play;A;1082;;43.28;86.56;;;Equipable: Throwing;+4%;16%|"+
-"A Priest, a Monk, and a Paladin;A;2172;108.6;86.88;86.88;86.88;;Equipable: Mace;+4%;16%|"+
-"Shopping for Medicine;A;;54.1;;;86.56;43.28;White Magic: 7;+4%;16%|"+
-"The Missing Key;A;2142;107.1;;;42.84;85.68;Black Magic: 7;+4%;16%|"+
-"Across the Desert;A;2142;53.55;;;85.68;85.68;Green Magic: 7;+4%;16%|"+
-"Cook Off;A;2389;119.45;95.56;;47.78;95.56;Series: FFXV;+4%;16%|"+
-"Tavern Cleanup;A;2429;121.45;97.16;97.16;97.16;97.16;Series: FFIII;+4%;16%|"+
-"Too Many Coeurls;A;2389;;47.78;95.56;95.56;95.56;Series: FFII;+4%;16%|"+
-"Lost in the Dark;A;1190;;95.2;47.6;;95.2;Series: FFI;+4%;16%|"+
-"Anger Management;A;2389;119.45;;95.56;95.56;47.78;White Magic: 7;+4%;16%|"+
-"Wrath in the Wreckage;A;2389;119.45;;95.56;47.78;95.56;Black Magic: 7;+4%;16%|"+
-"Seven Years Bad Luck;A;1190;59.5;;95.2;;95.2;Green Magic: 7;+4%;16%|"+
-"Ghost Busted;A;2389;119.45;;95.56;95.56;47.78;White Magic: 7;+4%;16%|"+
-"What's Her Secret?;A;2389;119.45;;95.56;47.78;95.56;Black Magic: 7;+4%;16%|"+
-"Braving the Elements;A;1190;59.5;;95.2;;95.2;Green Magic: 7;+4%;16%|"+
-"Lost in the World;S;2298;229.8;91.92;91.92;;;Equipable: Great Sword;+3%;16%|"+
-"Hotline Grandshelt;S;4447;222.35;59.29;177.88;;177.88;Equipable: Katana;+3%;16%|"+
-"Break Time;S;4596;114.9;;;91.92;91.92;Equipable: Bow;+3%;16%|"+
-"Reinventing the Wheel;S;4575;228.75;91.5;91.5;;183;Equipable: Hammer;+3%;16%|"+
-"Winter is Coming;S;;114.9;;183.84;91.92;91.92;Equipable: Robe;+3%;16%|"+
-"Hide your wallets!;S;4447;222.35;59.29;177.88;;177.88;Equipable: Gun;+3%;16%|"+
-"Paper, Please!;S;4575;114.38;183;;91.5;183;Equipable: Spear;+3%;16%|"+
-"Wherefore Art Thou, Father;S;2298;229.8;91.92;91.92;;;Equipable: Great Sword;+3%;16%|"+
-"A \"Legendary\" Challenge;S;4447;222.35;59.29;177.88;;177.88;Equipable: Katana;+3%;16%|"+
-"Where's My Mommy?;S;4596;114.9;;;91.92;91.92;Equipable: Bow;+3%;16%|"+
-"Be the Best;S;4575;228.75;91.5;91.5;;183;Equipable: Hammer;+3%;16%|"+
-"Hot Ice;S;;114.9;;183.84;91.92;91.92;Equipable: Robe;+3%;16%|"+
-"Save the Children;S;4447;222.35;59.29;177.88;;177.88;Equipable: Gun;+3%;16%|"+
-"Magi in Danger;S;4575;114.38;183;;91.5;183;Equipable: Spear;+3%;16% |";
+var data = "Relic Hunting Craze I;D;1;;;;;;Series: FFBE;+15%;100%|"+
+"Relic Hunting Craze II;D;1;;;;;;Series: FFBE;+15%;50%|"+
+"Relic Hunting Craze III;D;1;;;;;;Series: FFBE;+15%;30%|"+
+"Strange Encounters;D;;;3.12;;;;Equippable: Dagger;+15%;30%|"+
+"Shirts and Skins;D;;;;3.12;;Equippable: Clothes;+15%;30%|"+
+"Put to the Test;D;;;3.12;;;;Equippable: Sword;+15%;30%|"+
+"Needle in a Haystack;D;;;;;3.12;;Equippable: Rod;+15%;30%|"+
+"Merchant Guard;D;104;;;4.16;;;Equippable: Light Shield;+15%;30%|"+
+"Guard Duty;D;104;;;4.16;;;Equippable: Heavy Shield;+15%;30%|"+
+"Mouse Trapped;D;;;;;;3.12;White Magic: 2;+15%;30%|"+
+"Farmer's Dismay;D;;;;;3.12;Black Magic: 2;+15%;30%|"+
+"Cause for Celebration;D;;3.9;;;;;Green Magic: 2;+15%;30%|"+
+"What Might Happen?;D;;;;;;3.12;White Magic: 2;+15%;30%|"+
+"Flame Fighters;D;;;;;3.12;;Black Magic: 2;+15%;30%|"+
+"Devilish Arms;D;;3.9;;;;;Green Magic: 2;+15%;30%|"+
+"Can't Beat Free;D;;;;;3.12;;Equippable: Hat;+15%;30%|"+
+"To the Rescue;D;;;;3.12;;;Equippable: Light Armor;+15%;30%|"+
+"Heavy Objects Collide;D;;;;3.12;;;Equippable: Heavy Armor;+15%;30%|"+
+"Lightning Rod;D;;;;;;3.12;Equippable: Helm;+15%;30%|"+
+"Dream Job;D;;5.2;;;;4.16;Series: FFBE;+15%;30%|"+
+"No Sheep to Count;D;;5.2;;;;4.16;Series: FFBE;+15%;30%|"+
+"Poker Tournament;D;;;;;4.16;4.16;Series: FF TYPE-0;+15%;30%|"+
+"Winter Retreat;D;104;5.2;;;;;Series: FFT;+15%;30%|"+
+"Buzzkill;C;;;7.76;;;;Equippable: Dagger;+10%;20%|"+
+"Just Cause;C;;17.25;;;13.8;;Equippable: Clothes;+10%;20%|"+
+"Bad to the Bone;C;;;13.8;13.8;;;Equippable: Sword;+10%;20%|"+
+"Volcano on Ice;C;;;;;7.76;;Equippable: Staff;+10%;20%|"+
+"Leave No Stones Unturned;C;;;7.76;;;;Equippable: Rod;+10%;20%|"+
+"Precious Lumber;C;345;;13.8;;;;Equippable: Axe;+10%;20%|"+
+"Test Your Strength;C;423;;16.92;16.92;;;Equippable: Fist;+10%;20%|"+
+"Targeted;C;423;;;16.92;;16.92;Equippable: Light Shield;+10%;20%|"+
+"Call to Arms;C;;;13.8;13.8;;;Equippable: Heavy Shield;+10%;20%|"+
+"Grand Theft Carriage;C;;17.25;;;;;13.8;;Equippable: Helm;+10%;20%|"+
+"Paging All White Mages;C;;;;;;7.76;White Magic: 3;+10%;20%|"+
+"The Mage-Off;C;;;;;7.76;;Black Magic: 3;+10%;20%|"+
+"Crab Battle;C;345;17.25;;;;;Green Magic: 3;+10%;20%|"+
+"None Shall Starve;C;;17.25;;;;13.8;White Magic: 3;+10%;20%|"+
+"Stop the Horde!;C;;;13.8;;13.8;;Black Magic: 3;+10%;20%|"+
+"Arms on Fire!;C;;;;13.8;;13.8;Green Magic: 3;+10%;20%|"+
+"Lost in Fantasy;C;423;;;16.92;;16.92;Series: FFBE;+10%;20%|"+
+"Time to Relax;C;423;;;16.92;;16.92Series: FFBE;+10%;20%|"+
+"Parley?;C;;;8.62;17.24;;;Series: FFXII;+10%;20%|"+
+"Lightning Returns?;C;;;;;8.62;17.24Series: FFXIII;+10%;20%|"+
+"Plumbers Paradise;B;;;39.72;39.72;;39.72;White Magic: 5;+6%;16%|"+
+"Spelunking Splendor;B;993;;39.72;;39.72;;Black Magic: 5;+6%;16%|"+
+"Will to Survive;B;505.5;50.55;;;;;Green Magic: 5;+6%;16%|"+
+"Unpleasant Dreams;B;;49.65;;;39.72;39.72;White Magic: 5;+6%;16%|"+
+"Egg Hunt;B;993;;39.72;;39.72;;Black Magic: 5;+6%;16%|"+
+"Back to the Sands;B;;49.65;;;39.72;39.72;Green Magic: 5;+6%;16%|"+
+"Weathering the Weather;B;993;49.65;;;;39.72;Equippable: Staff;+6%;16%|"+
+"Quarry Quickness;B;505.5;;40.44;;;;Equippable: Axe;+6%;16%|"+
+"Cat Lost;B;1159;28.98;;;;46.36;Equippable: Harp;+6%;16%|"+
+"Monster Tamer;B;;49.65;39.72;;39.72;;Equippable: Whip;+6%;16%|"+
+"Run, Cactuar, Run;B;;;20.22;;40.44;;Equippable: Throwing;+6%;16%|"+
+"A Certain Set of Skills;B;1011;;20.22;;;;Equippable: Mace;+6%;16%|"+
+"Basher's Brawl;B;993;;39.72;39.72;;;Equippable: Fist;+6%;16%|"+
+"Undead Rising;B;;50.55;;;;20.22;White Magic: 5;+6%;16%|"+
+"Will It Hold?;B;;50.55;;;20.22;;Black Magic: 5;+6%;16%|"+
+"Crying Over Spilt Milk;B;993;49.65;;;;39.72;Green Magic: 5;+6%;16%|"+
+"Sword in the Stone;B;1159;;23.18;46.36;;;Series: FFIV;+6%;16%|"+
+"Join the Circus;B;1165;58.25;46.6;46.6;;;Series: FFIX;+6%;16%|"+
+"Hot Topic;B;;57.95;;;23.18;46.36;Series: FFVI;+6%;16%|"+
+"Impact from Above;B;;;46.6;46.6;46.6;46.6;Series: FFV;+6%;16%|";
 
 var result = "";
 var lines = data.split("|");
