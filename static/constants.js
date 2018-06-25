e7507491dbc8d25f413f527f7aae2d77020156c6
Jonathan Dion <jonathan.dion@agfa.com>
2018-06-25 10:10:20 +0200

    IW

--- static/constants.js
+++ static/constants.js
@@ -62,3 +62,29 @@ const itemEnhancementLabels = {
     "autoProtect_5": "Auto-Protect 5","autoProtect_4": "Auto-Protect 4","autoProtect_3": "Auto-Protect 3","autoProtect_2": "Auto-Protect 2","autoProtect_1": "Auto-Protect 1",
     "autoShell_5": "Auto-Shell 5","autoShell_4": "Auto-Shell 4","autoShell_3": "Auto-Shell 3","autoShell_2": "Auto-Shell 2","autoShell_1": "Auto-Shell 1",
 };
+const itemEnhancementAbilities = {
+    "rare":{
+        "dagger": {"lbFillRate":100}, 
+        "sword": {"hp%":15, "atk%":15}, 
+        "greatSword": {"hp%":15, "atk%":15}, 
+        "katana": {"hp%":15, "atk%":15}, 
+        "staff": {"mp%":15, "spr%":15}, 
+        "rod": {"mp%":15, "mag%":15}, 
+        "bow": {"atk%":15, "mag%":15}, 
+        "axe": {"atk%":30}, 
+        "hammer": {"hp%":15, "def%":15}, 
+        "spear": {"hp%":15, "atk%":15}, 
+        "harp": {"hp%":15, "spr%":15}, 
+        "whip": {"mp%":30}, 
+        "throwing": {"hp%":15, "mp%":15}, 
+        "gun": {"atk%":30}, 
+        "mace": {"hp%":30}, 
+        "fist": {"hp%":15, "atk%":15}
+    },
+    "hp_15": {"hp%":15}, "hp_12": {"hp%":12}, "hp_10": {"hp%":10}, "hp_7": {"hp%":7}, "hp_5": {"hp%":5}, "hp_3": {"hp%":3}, "hp_1": {"hp%":1},
+    "mp_15": {"mp%":15}, "mp_12": {"mp%":12}, "mp_10": {"mp%":10}, "mp_7": {"mp%":7}, "mp_5": {"mp%":5}, "mp_3": {"mp%":3}, "mp_1": {"mp%":1},
+    "atk_15": {"atk%":15}, "atk_12": {"atk%":12}, "atk_10": {"atk%":10}, "atk_7": {"atk%":7}, "atk_5": {"atk%":5}, "atk_3": {"atk%":3}, "atk_1": {"atk%":1},
+    "def_15": {"def%":15}, "def_12": {"def%":12}, "def_10": {"def%":10}, "def_7": {"def%":7}, "def_5": {"def%":5}, "def_3": {"def%":3}, "def_1": {"def%":1},
+    "mag_15": {"mag%":15}, "mag_12": {"mag%":12}, "mag_10": {"mag%":10}, "mag_7": {"mag%":7}, "mag_5": {"mag%":5}, "mag_3": {"mag%":3}, "mag_1": {"mag%":1},
+    "spr_15": {"spr%":15}, "spr_12": {"spr%":12}, "spr_10": {"spr%":10}, "spr_7": {"spr%":7}, "spr_5": {"spr%":5}, "spr_3": {"spr%":3}, "spr_1": {"spr%":1}
+}
