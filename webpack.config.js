50dad39278632543196382718396d1b2abd3fca4
Arnaud Dezandee <dezandee.arnaud@gmail.com>
2018-08-24 11:59:19 +0200

    feat(webpack): add css cache busting - minify

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const cssnano = require('cssnano');
+const glob = require('glob');
+
+module.exports = {
+  entry: glob.sync('./static/*.html'),
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: './static/',
+            },
+          },
+          {
+            loader: 'extract-loader',
+            options: {},
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['link:href'],
+              minimize: true,
+            },
+          }],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]',
+              context: './static/',
+              emitFile: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[path][name].[ext]?[hash:8]',
+              context: './static/',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [cssnano()],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '',
+    filename: 'app.js',
+  },
+};
