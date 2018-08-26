508889e2d8f5a60e99020e047114a3900c20d55c
Guillaume <contact-github@gperrin.fr>
2018-08-26 15:15:16 +0200

    (Server) Route/Correction: always return JSON

--- server/routes/corrections.js
+++ server/routes/corrections.js
@@ -8,34 +8,39 @@ route.post('/:server/corrections', async (req, res) => {
     var validator = new jv.JSONValidation();
     var data = req.body;
     if (Object.keys(data).length > 10) {
-        res.status(400).send("Only a maximum of 10 corrections can be send at a time");
+        res.status(400).send({error: "Only a maximum of 10 corrections can be send at a time"});
         return;
     }
     for (var key in data) {
         if (isNaN(parseInt(key))) { 
-            res.status(400).send(key + " is not valid as an item key");
+            res.status(400).send({error: key + " is not valid as an item key"});
             return;
         }
         var result = validator.validate(data[key], schemaData);
         if (!result.ok) {
-            res.status(400).send("JSON has the following errors: " + result.errors.join(", ") + " at path " + result.path);
+            res.status(400).send({error: "JSON has the following errors: " + result.errors.join(", ") + " at path " + result.path});
             return;
         }
     }
     
     var fileName = 'static/' + req.params.server + '/corrections.json';
     var tempItems = {};
+    var modifiedItems = 0;
     if (fs.existsSync(fileName)) {
         tempItems = JSON.parse(fs.readFileSync(fileName, 'utf8'));
     }
     for (var key in data) {
         tempItems[key] = data[key];
+        modifiedItems++;
     }
     if (Object.keys(tempItems).length > 2000) {
-        res.status(500).send("The file is too big on the server");
+        res.status(500).send({error: "The file is too big on the server"});
     } else {
         fs.writeFileSync(fileName, JSON.stringify(tempItems).replace(/\},\{/g, '},\n\t{').replace(/^\[/g, '[\n\t').replace(/\]$/g, '\n]'));
-        res.status(201).send();
+        res.status(201).send({
+            modified: modifiedItems,
+            total: Object.keys(tempItems).length
+        });
     }
     
 });
