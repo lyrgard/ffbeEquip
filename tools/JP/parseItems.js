915e216b62130ef1aadca47bfc88e01fa96ddbcc
Jonathan Dion <jonathan.dion@agfa.com>
2018-12-20 14:45:50 +0100

    UPDATE_DATA JP data updated

--- tools/JP/parseItems.js
+++ tools/JP/parseItems.js
@@ -806,7 +806,7 @@ function formatOutput(items) {
 function verifyImage(icon) {
     var filePath = "../../static/img/items/" + icon;
     if (!fs.existsSync(filePath)) {
-        download("http://diffs.exvius.gg/asset_files/ja/item_item1/58/" + icon ,filePath);
+        download("http://diffs.exvius.gg/asset_files/ja/item_item1/59/" + icon ,filePath);
     }
 }
 
