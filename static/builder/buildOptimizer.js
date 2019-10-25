9a81d572e7f67af3d67c70abe6097a41cefc555e
Lyrgard <jonathan.dion@agfa.com>
2019-10-25 08:56:29 +0200

    WIP

--- static/builder/buildOptimizer.js
+++ static/builder/buildOptimizer.js
@@ -7,6 +7,19 @@ class BuildOptimizer {
         this.buildCounterUpdateCallback = buildCounterUpdateCallback;
     }
     
+    const typeByIndex = {
+        0: 'hand',
+        1: 'hand',
+        2: 'head',
+        3: 'body',
+        4: 'accessory',
+        5: 'accessory',
+        6: 'materia',
+        7: 'materia',
+        8: 'materia',
+        9: 'materia'
+    }
+    
     set unitBuild(unitBuild) {
         this._unitBuild = unitBuild;
         this.desirableElements = [];
@@ -29,71 +42,43 @@ class BuildOptimizer {
         }
     }
     
+    set itemPools(itemPools) {
+        this._itemPools = itemPools;
+    }
+    
     set alreadyUsedEspers(alreadyUsedEspers) {
         this._alreadyUsedEspers = alreadyUsedEspers;
     }
     
-    optimizeFor(typeCombinations, betterBuildFoundCallback) {
+    optimizeFor(betterBuildFoundCallback) {
         this.buildCounter = 0;
         this.elementalConditionItemsByType = {};
         this.betterBuildFoundCallback = betterBuildFoundCallback;
-        this.itemConditionalElements = {};
-        this.dataWithCondition.forEach(entry => {
-            entry.item.equipedConditions.forEach(condition => {
-                if (elementList.includes(condition)) {
-                    if (!this.itemConditionalElements[condition]) {
-                        this.itemConditionalElements[condition] = [];
-                    }
-                    this.itemConditionalElements[condition].push(entry);
-                }
-            })
-        })
+        
         var combinationsNumber = typeCombinations.length;
         for (var index = 0, len = combinationsNumber; index < len; index++) {
 
-            var dataWithdConditionItems = {}
-            for (var slotIndex = 0; slotIndex < 10; slotIndex++) {
-                if (typeCombinations[index].combination[slotIndex] && !dataWithdConditionItems[typeCombinations[index].combination[slotIndex]]) {
-                    dataWithdConditionItems[typeCombinations[index].combination[slotIndex]] = this.selectItems(typeCombinations[index].combination[slotIndex], typeCombinations[index].combination, typeCombinations[index].fixedItems, typeCombinations[index].forcedItems, this.dataWithCondition);
-                }
-            }
             var applicableSkills = [];
             for (var skillIndex = this._unitBuild.unit.skills.length; skillIndex--;) {
                 var skill = this._unitBuild.unit.skills[skillIndex];
-                if (this.areConditionOKBasedOnTypeCombination(skill, typeCombinations[index].combination, this._unitBuild._level)) {
-                    applicableSkills.push(skill);
-                }
+                applicableSkills.push(skill);
             }
             
             if (this.useEspers) {
-                this.selectedEspers = this.selectEspers(this._alreadyUsedEspers, this.ennemyStats, typeCombinations[index].combination);
+                this.selectedEspers = this.selectEspers(this._alreadyUsedEspers, this.ennemyStats);
             } else {
                 this.selectedEspers = [];
             }
             
             var build = [null, null, null, null, null, null, null, null, null, null,null].concat(applicableSkills);
-            this.findBestBuildForCombination(0, build, typeCombinations[index].combination, dataWithdConditionItems, typeCombinations[index].fixedItems, this.getElementBasedSkills(), this.getItemBasedSkills());
+            this.findBestBuild(10, build);
         }
         this.buildCounterUpdateCallback(this.buildCounter);
     }
     
-    selectEspers(alreadyUsedEspers, ennemyStats, typeCombination) {
+    selectEspers(alreadyUsedEspers, ennemyStats) {
         var selectedEspers = [];
         let espersToUse = {};
-        Object.keys(this.espers).forEach(name => {
-            let e = this.espers[name];
-            if (e.conditional && e.conditional.some(c => typeCombination.includes(c.equipedCondition))) {
-                e = JSON.parse(JSON.stringify(e));
-                e.conditional.filter(c => typeCombination.includes(c.equipedCondition)).forEach(c => {
-                   baseStats.forEach(s => {
-                       if (c[s+'%']) {
-                           addToStat(e, s+'%', c[s+'%']);
-                       }
-                   }) 
-                });
-            }
-            espersToUse[name] = e;
-        })
         var keptEsperRoot = EsperTreeComparator.sort(espersToUse, alreadyUsedEspers, this._unitBuild.involvedStats, ennemyStats);
         for (var index = keptEsperRoot.children.length; index--;) {
             if (!selectedEspers.includes(keptEsperRoot.children[index])) {
@@ -101,90 +86,6 @@ class BuildOptimizer {
             }
         }
         return selectedEspers;
-    }
-    
-    selectItems(type, typeCombination, fixedItems, forcedItems, dataWithCondition) {
-        
-        var itemsOfType = this.dataByType[type];
-        var dataWithoutConditionIds = [];
-        for (var index = itemsOfType.length; index--; ) {
-            if (!dataWithoutConditionIds.includes(itemsOfType[index].item.id)) {
-                dataWithoutConditionIds.push(itemsOfType[index].item.id);
-            }
-        }
-        var tempResult = itemsOfType.slice();
-        var dataWithConditionKeyAlreadyAddedOwned = [];
-        var dataWithConditionKeyAlreadyAddedNotOwned = [];
-        for (var index = 0, len = this.dataWithCondition.length; index < len; index++) {
-            var entry = this.dataWithCondition[index];
-            var item = entry.item;
-            if (item.type == type && ((entry.owned && !dataWithConditionKeyAlreadyAddedOwned.includes(item.id)) || (!entry.owned && !dataWithConditionKeyAlreadyAddedNotOwned.includes(item.id))))  {
-                var allFound = true;
-                for (var conditionIndex in item.equipedConditions) {
-                    let condition = item.equipedConditions[conditionIndex]
-                    if (!typeCombination.includes(condition)) {
-                        allFound = false;
-                        break;
-                    }
-                }
-                if (allFound) {
-                    if (dataWithoutConditionIds && dataWithoutConditionIds.includes(entry.item.id)) {
-                        // If we add a condition items that have a none conditioned version alreadty selected, remove that second version
-                        for (var alreadyAddedIndex = tempResult.length; alreadyAddedIndex--;) {
-                            if (tempResult[alreadyAddedIndex].item.id == entry.item.id) {
-                                tempResult.splice(alreadyAddedIndex,1);
-                                break;    
-                            }
-                        }
-                    }
-
-                    tempResult.push(entry);
-                    if (entry.owned) {
-                        dataWithConditionKeyAlreadyAddedOwned.push(item.id);    
-                    } else {
-                        dataWithConditionKeyAlreadyAddedNotOwned.push(item.id);    
-                    }
-                    
-
-                }
-            }
-        }
-        
-        var numberNeeded = 0;
-        for (var slotIndex = typeCombination.length; slotIndex--;) {
-            if (typeCombination[slotIndex] == type && !fixedItems[slotIndex]) {
-                numberNeeded++;
-            }
-        }
-        var includeSingleWielding = !typeCombination[0] || !typeCombination[1];
-        var includeDualWielding = typeCombination[0] && typeCombination[1] && weaponList.includes(typeCombination[0]) && weaponList.includes(typeCombination[1]);
-        var fixedString = "";
-        for (var i = 0; i < fixedItems.lenght; i++) {
-            if (fixedItems[i]) {
-                fixedString += ' - ' + fixedItems[i].name;
-            }
-        }
-        var skillsIds = getSkillIds(this._unitBuild.formula) || [];
-        var itemPool = new ItemPool(numberNeeded, this._unitBuild.involvedStats, this.ennemyStats, this.desirableElements, this.desirableItemIds, skillsIds, includeSingleWielding, includeDualWielding);
-        for (var i = tempResult.length; i--;) {
-            var entry = tempResult[i];
-            if (weaponList.includes(type) && (typeCombination[1] || this._unitBuild.fixedItems[0] || this._unitBuild.fixedItems[1]) && isTwoHanded(entry.item) ) {
-                continue; // ignore 2 handed weapon if we are in a DW build, or a weapon was already fixed
-            }
-            let available = entry.available;
-            if (forcedItems.includes(entry.item.id)) {
-                available--;
-                if (available <= 0) {
-                    continue;
-                }
-            }
-            itemPool.addItem(entry, available);
-        }
-        
-        itemPool.prepare();
-        /*var resultTree =  ItemTreeComparator.sort(tempResult, numberNeeded, this._unitBuild, this.ennemyStats, this.desirableElements, this.desirableItemIds, typeCombination, includeSingleWielding, includeDualWielding);
-        return resultTree;*/
-        return itemPool;
     }
     
     getElementBasedSkills() {
@@ -219,114 +120,30 @@ class BuildOptimizer {
         return itemBasedSkills;
     }
     
-    areConditionOKBasedOnTypeCombination(item, typeCombination, level) {
-        if (level && item.levelCondition && item.levelCondition > level) {
-            return false;
-        }
-        if (item.equipedConditions) {
-            for (var conditionIndex = item.equipedConditions.length; conditionIndex--;) {
-                if (elementList.includes(item.equipedConditions[conditionIndex])) {
-                    return false;
-                } else {
-                    if (!typeCombination.includes(item.equipedConditions[conditionIndex])) {
-                        return false;
-                    }
-                }
-            }
-        }
-        return true;
-    }
-    
-    
-    
-    findBestBuildForCombination(index, build, typeCombination, dataWithConditionItems, fixedItems, elementBasedSkills, itemBasedSkills) {
+    findBestBuild(index, build) {
         let restorePool;
         let savedItemPools;
-        if (index == 2) {
-            savedItemPools = [];
-            savedItemPools.push(dataWithConditionItems[typeCombination[2]]);
-            savedItemPools.push(dataWithConditionItems[typeCombination[3]]);
-            savedItemPools.push(dataWithConditionItems[typeCombination[4]]);
-            savedItemPools.push(dataWithConditionItems[typeCombination[6]]);
-            // weapon set, try elemental based skills
-            for (var skillIndex = elementBasedSkills.length; skillIndex--;) {
-                if (build.includes(elementBasedSkills[skillIndex])) {
-                    if (!areConditionOK(elementBasedSkills[skillIndex], build)) {
-                        build.splice(build.indexOf(elementBasedSkills[skillIndex]),1);
-                    }
-                } else {
-                    if (areConditionOK(elementBasedSkills[skillIndex], build)) {
-                        build.push(elementBasedSkills[skillIndex]);
-                    }
-                }
-            }
-            // Try to see if elemental items are activated
-            let activatedItemsByType = {};
-            let activatedItemIds = [];
-            if (build[0] && build[0].element) {
-                build[0].element.forEach(element => {
-                   if (this.itemConditionalElements[element]) {
-                       this.itemConditionalElements[element].forEach(entry => {
-                           if (!activatedItemIds.includes(entry.item.id)) {
-                               if (!activatedItemsByType[entry.item.type]) {
-                                   activatedItemsByType[entry.item.type] = [];
-                               }
-                               activatedItemsByType[entry.item.type].push(entry);
-                               activatedItemIds.push(entry.item.id);
-                           }
-                       })
-                   }
-                });
-            }
-            if (build[1] && build[1].element) {
-                build[1].element.forEach(element => {
-                    if (this.itemConditionalElements[element]) {
-                        this.itemConditionalElements[element].forEach(entry => {
-                            if (!activatedItemIds.includes(entry.item.id)) {
-                                if (!activatedItemsByType[entry.item.type]) {
-                                    activatedItemsByType[entry.item.type] = [];
-                                }
-                                activatedItemsByType[entry.item.type].push(entry);
-                                activatedItemIds.push(entry.item.id);
-                            }
-                        })
-                    }
-                });
-            }
-            if (Object.keys(activatedItemsByType).length > 0) {
-                Object.keys(activatedItemsByType).forEach(type => {
-                    restorePool = true;
-                    if (dataWithConditionItems[type]) {
-                        dataWithConditionItems[type] = dataWithConditionItems[type].clone();
-                        dataWithConditionItems[type].addItems(activatedItemsByType[type]);
-                        dataWithConditionItems[type].prepare();
-                    }
-                });
-            }
-        }
+        
+        
 
-        if (fixedItems[index]) {
-            this.tryItem(index, build, typeCombination, dataWithConditionItems, fixedItems[index], fixedItems,elementBasedSkills, itemBasedSkills);
+        if (this._unitBuild.fixedItems[index]) {
+            this.tryItem(index, build, this._unitBuild.fixedItems[index]);
         } else {
-            if (typeCombination[index]) {
-                let itemPool = dataWithConditionItems[typeCombination[index]];
-                var foundAnItem = false;
-                for (var i = itemPool.keptItems.length; i--;) {
-                    if (itemPool.keptItems[i].active) {
-                        var item = itemPool.take(i);
-                        this.tryItem(index, build, typeCombination, dataWithConditionItems, item, fixedItems, elementBasedSkills, itemBasedSkills);
-                        itemPool.putBack(i);
-                        foundAnItem = true;
-                    }
+            let itemPool = this._itemPools[typeByIndex[index]];
+            var foundAnItem = false;
+            for (var i = itemPool.keptItems.length; i--;) {
+                if (itemPool.keptItems[i].active) {
+                    var item = itemPool.take(i);
+                    this.tryItem(index, build, typeCombination, dataWithConditionItems, item, fixedItems, elementBasedSkills, itemBasedSkills);
+                    itemPool.putBack(i);
+                    foundAnItem = true;
                 }
-            
-                if (!foundAnItem) {
-                    this.tryItem(index, build, typeCombination, dataWithConditionItems, {"name":"Any " + typeCombination[index],"type":typeCombination[index], "placeHolder":true}, fixedItems, elementBasedSkills, itemBasedSkills);
-                }
-                build[index] == null;
-            } else {
-                this.tryItem(index, build, typeCombination, dataWithConditionItems, null, fixedItems, elementBasedSkills, itemBasedSkills);
             }
+
+            if (!foundAnItem) {
+                this.tryItem(index, build, null);
+            }
+            build[index] == null;
         }
         if (restorePool) {
             build[index] = null;
@@ -337,7 +154,7 @@ class BuildOptimizer {
         }
     }
 
-    tryItem(index, build, typeCombination, dataWithConditionItems, item, fixedItems, elementBasedSkills, itemBasedSkills) {
+    tryItem(index, build, item) {
         if (index == 0 && item && isTwoHanded(item) && typeCombination[1]) {
             return; // Two handed weapon only accepted on DH builds
         }
@@ -381,7 +198,7 @@ class BuildOptimizer {
                 }
             }
         } else {
-            this.findBestBuildForCombination(index + 1, build, typeCombination, dataWithConditionItems, fixedItems, elementBasedSkills, itemBasedSkills);
+            this.findBestBuild(index - 1, build);
         }
     }
 
