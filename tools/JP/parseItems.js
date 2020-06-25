a1a7a1e34892c288627ee8840f8d363b397d0970
Lyrgard <lyrgard@gmail.com>
2020-06-25 22:59:38 +0200

    updated GL & JP data

--- tools/JP/parseItems.js
+++ tools/JP/parseItems.js
@@ -110,6 +110,7 @@ const unitRules = {
     4008: (item) => item.exclusiveUnits = nvUnitIdsByGameId[10007], // FF7 units
     7102: (item) => item.exclusiveSex = 'male',// Male units
     7103: (item) => item.exclusiveSex = 'female',// Female units,
+    7212: (item) => item.exclusiveUnits = nvUnitIdsByGameId[10012], // FF12
     7216: (item) => item.exclusiveUnits = nvUnitIdsByGameId[11001], // FFBE units
 }
 
