17458402ca56bbfdce67a07b616b6a4618326b61
Ben Sailors <sailorsben@gmail.com>
2022-08-11 10:03:45 -0600

    Added async await

--- server/middlewares/oauth.js
+++ server/middlewares/oauth.js
@@ -1,6 +1,6 @@
 import OAuth  from "../lib/oauth.js";
 
-export function OAuthFunction(req, res, next){
+export async function OAuthFunction(req, res, next){
   const { tokens } = req.OAuthSession;
   if (!tokens) {
     console.log("No tokens found...")
@@ -10,7 +10,7 @@ export function OAuthFunction(req, res, next){
   console.log("Tokens found")
   req.OAuth2Client = OAuth.createClient(tokens);
 
-  return req.OAuth2Client.getRequestMetadataAsync(null, (error) => {
+  return await req.OAuth2Client.getRequestMetadata(null, (error) => {
     req.OAuthSession.tokens = req.OAuth2Client.credentials;
     console.log(req.OAuthSession.tokens)
     next(error);
