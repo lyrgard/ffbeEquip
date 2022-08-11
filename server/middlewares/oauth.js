6929479932be354c2ea58a6da470b3179f2379e6
Ben Sailors <sailorsben@gmail.com>
2022-08-11 11:04:08 -0600

    Small rewrite to MetadataAsync

--- server/middlewares/oauth.js
+++ server/middlewares/oauth.js
@@ -10,7 +10,13 @@ export function OAuthFunction(req, res, next){
   console.log("Tokens found")
   req.OAuth2Client = OAuth.createClient(tokens);
 
-  return req.OAuth2Client.getRequestMetadataAsync(null);
+  let output = req.OAuth2Client.getRequestMetadataAsync(null, (error) => {
+    req.OAuthSession.tokens = req.OAuth2Client.credentials;
+    console.log(req.OAuthSession.tokens)
+    next(error);
+  });
+
+  return output
 };
 
 export default { OAuthFunction }
