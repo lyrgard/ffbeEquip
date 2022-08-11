fe9981af8a8da35d67bde461bf630fb7b5d48185
Ben Sailors <sailorsben@gmail.com>
2022-08-10 22:22:32 -0600

    More troubleshooting

--- server.js
+++ server.js
@@ -139,6 +139,7 @@ if (config.firebase.enabled) {
     app.use('/', authRequired, firebase.authenticatedRoute);
 }
 if (config.google.enabled) {
+    console.log("Google Drive is enabled")
     app.use('/', authRequired, drive);
 }
 
