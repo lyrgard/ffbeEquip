cface5ad1632658a5ea2dfb04d4e23262a038477
Ben Sailors <sailorsben@gmail.com>
2022-08-12 14:15:00 -0600

    Logging

--- server/middlewares/oauth.js
+++ server/middlewares/oauth.js
@@ -8,6 +8,9 @@ export function OAuthFunction(req, res, next){
 
   req.OAuth2Client = OAuth.createClient(tokens);
 
+  console.log("Tokens exist.")
+  console.log(tokens)
+
   return req.OAuth2Client.getRequestMetadataAsync(null, (error) => {
     req.OAuthSession.tokens = req.OAuth2Client.credentials;
     next(error);
