fffa255bb586e7bd796c27a3c39dc8b8872ce38a
Ben Sailors <sailorsben@gmail.com>
2022-08-11 21:54:47 -0600

    hardcoded URL

--- server/routes/drive.js
+++ server/routes/drive.js
@@ -25,17 +25,17 @@ route.get('/:server/userData', async (req, res) => {
   return res.status(200).json(result);
 });
 
-route.get('/:server/:table', async (req, res) => {
+route.get('/GL/itemInventory', async (req, res) => {
   console.log(`${table}_${server}.json`)
-  // const { server, table } = req.params;
-  // const auth = req.OAuth2Client;
+  const { server, table } = req.params;
+  const auth = req.OAuth2Client;
 
-  // let db = await drive.readJson(auth, `${table}_${server}.json`, {});
-  // if (db.version < DB_VERSION) {
-  //   db = migration.up(table, db);
-  // }
+  let db = await drive.readJson(auth, `${table}_${server}.json`, {});
+  if (db.version < DB_VERSION) {
+    db = migration.up(table, db);
+  }
 
-  // return res.status(200).json(db);
+  return res.status(200).json(db);
 });
 
 
