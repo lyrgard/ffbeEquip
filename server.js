e849613c3ba3ba89fb02e916417d63f09e2c8177
Ben Sailors <sailorsben@gmail.com>
2022-08-11 23:21:06 -0600

    Google Enabled message

--- server.js
+++ server.js
@@ -139,8 +139,7 @@ if (config.firebase.enabled) {
     app.use('/', authRequired, firebase.authenticatedRoute);
 }
 if (config.google.enabled) {
-    console.log("Google is enabled.")
-    app.use('/', drive);
+    app.use('/', authRequired, drive);
 }
 
 // Old index.html file no longer exists
