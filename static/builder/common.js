e7c3557ebb9ad57f33ebfcc916f550652224fc6c
Ben Sailors <sailorsben@gmail.com>
2022-09-13 20:58:34 -0500

    Dataversion

--- static/builder/common.js
+++ static/builder/common.js
@@ -62,7 +62,7 @@ function getValue(item, valuePath, notStackableSkillsAlreadyUsed) {
     if (notStackableSkillsAlreadyUsed && item.notStackableSkills) {
         for (var index = notStackableSkillsAlreadyUsed.length; index--;) {
             if (item.notStackableSkills[notStackableSkillsAlreadyUsed[index]]) {
-                console.log(notStackableSkillsAlreadyUsed)
+                //console.log(notStackableSkillsAlreadyUsed)
                 value -= getValue(item.notStackableSkills[notStackableSkillsAlreadyUsed[index]], valuePath);
                 //value -= getValue(item.notStackableSkills[notStackableSkillsAlreadyUsed[index]].staticStats, valuePath);
             }
