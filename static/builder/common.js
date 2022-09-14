c32b04e7d17d8912b435449d9eee8c1f95f87132
Ben Sailors <sailorsben@gmail.com>
2022-09-13 20:56:42 -0500

    Rollback for Accessory change

--- static/builder/common.js
+++ static/builder/common.js
@@ -62,8 +62,9 @@ function getValue(item, valuePath, notStackableSkillsAlreadyUsed) {
     if (notStackableSkillsAlreadyUsed && item.notStackableSkills) {
         for (var index = notStackableSkillsAlreadyUsed.length; index--;) {
             if (item.notStackableSkills[notStackableSkillsAlreadyUsed[index]]) {
+                console.log(notStackableSkillsAlreadyUsed)
                 value -= getValue(item.notStackableSkills[notStackableSkillsAlreadyUsed[index]], valuePath);
-                value -= getValue(item.notStackableSkills[notStackableSkillsAlreadyUsed[index]].staticStats, valuePath);
+                //value -= getValue(item.notStackableSkills[notStackableSkillsAlreadyUsed[index]].staticStats, valuePath);
             }
         }
     }
